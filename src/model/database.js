45dab77f436c9a067f73c2709f227f3805d31f34
Darren Haken <darrenhaken@gmail.com>
2018-07-31 16:42:17 +0100

    Also retrieve tables

---
+++ src/model/database.js
@@ -0,0 +1,8 @@
+class Database {
+  constructor(name, tables) {
+    this.name = name;
+    this.tables = tables;
+  }
+}
+
+module.exports = Database;
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router({});
+const {Glue} = require('aws-sdk');
+
+const Database = require('../model/database');
 const asyncMiddleware = require('../utils/asyncMiddleware');
 const logger = require('../utils/logger');
-const {Glue} = require('aws-sdk');
 
 const glue = new Glue();
 
 router.get('/', asyncMiddleware(async (req, res) => {
   logger.debug('Get AWS Glue databases');
-  const databases = await glue.getDatabases().promise();
-  res.json(databases);
+  const databaseResponse = await glue.getDatabases().promise();
+  const dbPromises = databaseResponse.DatabaseList.map(async db => {
+    const tables = await glue.getTables({DatabaseName: db.Name}).promise();
+    return new Database(db.Name, tables);
+  });
+  const dbs = await Promise.all(dbPromises);
+  res.json(dbs);
 }));
 
 module.exports = router;
